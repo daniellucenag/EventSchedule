e2246bdc48fa4d5a9a1bc8857fca25d5623261fa
Daniel Lucena <daniellucenag@gmail.com>
2020-02-12 21:21:09 -0300

    add some tests and build class to help with mocks

---
+++ EventSchedule.Test/Application/Event/EventAppServiceTest.cs
@@ -0,0 +1,66 @@
+﻿using EventSchedule.Application.Event;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EventSchedule.Test.Application.Event
+{
+    [TestClass]
+    public class EventAppServiceTest
+    {
+        [TestMethod]
+        public void ShouldReturnListOfEvents()
+        {
+            Mock<IEventAppService> mockEventAppService = new Mock<IEventAppService>();
+
+            EventBuilder builder = new EventBuilder().GetValidInstance();
+
+            mockEventAppService.Setup(o => o.GetAll()).Returns(builder.AllEvents);
+
+            var result = mockEventAppService.Object.GetAll();
+           
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ShouldNotReturnListOfEvents()
+        {
+            Mock<IEventAppService> mockEventAppService = new Mock<IEventAppService>();
+
+            EventBuilder builder = new EventBuilder().GetValidInstance();
+
+            mockEventAppService.Setup(o => o.GetAll()).Returns(builder.EmptyList);
+
+            var result = mockEventAppService.Object.GetAll();
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void ShouldReturnEventById()
+        {
+            Mock<IEventAppService> mockEventAppService = new Mock<IEventAppService>();
+
+            EventBuilder builder = new EventBuilder().GetValidInstance();
+
+            mockEventAppService.Setup(o => o.GetById(builder.SingleEvent.EventId)).Returns(builder.SingleEvent);
+
+            var result = mockEventAppService.Object.GetById(builder.SingleEvent.EventId);
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ShouldNotReturnEventById()
+        {
+            Mock<IEventAppService> mockEventAppService = new Mock<IEventAppService>();
+
+            EventBuilder builder = new EventBuilder().GetValidInstance();
+
+            mockEventAppService.Setup(o => o.GetById(builder.SingleEvent.EventId)).Returns(builder.EmptyEvent);
+
+            var result = mockEventAppService.Object.GetById(builder.SingleEvent.EventId);
+
+            Assert.IsNull(result);
+        }
+    }
+}
---
+++ EventSchedule.Test/Application/Event/EventBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventSchedule.Test.Application.Event
+{
+    public class EventBuilder
+    {
+        public IEnumerable<Core.Event.Event> AllEvents { get; set; }
+        public Core.Event.Event SingleEvent { get; set; }
+        public IEnumerable<Core.Event.Event> EmptyList {get;set;}
+        public Core.Event.Event EmptyEvent { get; set; }
+
+        public EventBuilder GetValidInstance()
+        {
+            return new EventBuilder()
+            {
+                AllEvents = new List<Core.Event.Event>()
+                {
+                    new Core.Event.Event()
+                    {
+                        EventId = 1,
+                        EventStart = DateTime.Now,
+                        EventEnd = DateTime.Now.AddDays(1),
+                        Description = "First of the list",
+                    },
+                    new Core.Event.Event()
+                    {
+                        EventId = 2,
+                        EventStart = DateTime.Now.AddDays(2),
+                        EventEnd = DateTime.Now.AddDays(3),
+                        Description = "Second of the list",
+                    }
+                },
+                SingleEvent = new Core.Event.Event()
+                {
+                    EventId = 3,
+                    EventStart = DateTime.Now,
+                    EventEnd = DateTime.Now.AddDays(2),
+                    Description = "First of single"
+                },
+                EmptyList = null,
+                EmptyEvent = null
+            };
+        }
+
+    }
+}
